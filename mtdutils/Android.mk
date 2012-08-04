d6d2edab61d9b4024804ab9bf395f9bcd87523a3
Jeff Kletsky <aokp@allycomm.com>
2012-08-03 21:37:40 -0700

    Can build and deliver bml_over_mtd
    target StaticExecutable: bml_over_mtd (out/target/product/galaxys4gmtd/obj/RECOVERY_EXECUTABLES/bml_over_mtd_intermediates/LINKED/bml_over_mtd)
    target Symbolic: bml_over_mtd (out/target/product/galaxys4gmtd/symbols/utilities/bml_over_mtd)
    target Strip: bml_over_mtd (out/target/product/galaxys4gmtd/obj/RECOVERY_EXECUTABLES/bml_over_mtd_intermediates/bml_over_mtd)
    Install: out/target/product/galaxys4gmtd/utilities/bml_over_mtd
    
    Change-Id: Id6086355cbc44d3fc6048db32ccd8e0e36ad08d4

--- mtdutils/Android.mk
+++ mtdutils/Android.mk
@@ -13,6 +13,23 @@ LOCAL_STATIC_LIBRARIES := libcutils libc
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 include $(BUILD_STATIC_LIBRARY)
 
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	bml_over_mtd.c
+
+LOCAL_MODULE := bml_over_mtd
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+
+LOCAL_STATIC_LIBRARIES := libmtdutils libcutils libc
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+
+include $(BUILD_EXECUTABLE)
+
+
 endif	# TARGET_ARCH == arm
 endif	# !TARGET_SIMULATOR
 
